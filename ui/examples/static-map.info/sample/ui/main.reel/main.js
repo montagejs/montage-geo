05eff4cf48667ac68de59b7c99dc8c1c37d83fee
Corentin Debost <codebost@gmail.com>
2020-09-14 11:41:45 -0700

    Add static map with arcgis imagery

---
+++ ui/examples/static-map.info/sample/ui/main.reel/main.js
@@ -0,0 +1,8 @@
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Main
+ * @extends Component
+ */
+exports.Main = Component.specialize(/** @lends Main.prototype */ {
+});
