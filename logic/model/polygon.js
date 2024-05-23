a5ce2d4d1db786c91113f21c0afb893e3f4391a6
Jonathan Miller <jonathan.miller@tenefit.com>
2024-05-23 12:09:46 -1000

    Fix syntax error

--- logic/model/polygon.js
+++ logic/model/polygon.js
@@ -535,7 +535,7 @@ var Polygon = exports.Polygon = Geometry.specialize(/** @lends Polygon.prototype
 
     MultiPolygon: {
         get: function () {
-            return require("logic/model/multi-polygon").MultiPolygon
+            return require("logic/model/multi-polygon").MultiPolygon;
         }
     },
 
